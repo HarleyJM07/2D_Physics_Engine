52ffc9287cc1e238d21163bdf2b2df3a71cfdeb0
Harley <HMdudejack@outlook.com>
2026-08-18 02:11:33 +0100

    Added mouse controls, can click to add a new ball, click and hold to drag a ball

--- demo.py
+++ demo.py
@@ -4,6 +4,8 @@ from vector2 import Vector2
 from rigid_body import RigidBody
 from world import World
 
+dragged_ball = None
+
 balls = []
 
 WIDTH, HEIGHT = 1500, 800
@@ -15,7 +17,7 @@ clock = pygame.time.Clock()
 
 world = World(floor_y=HEIGHT, ceiling_y=0, left_wall_x=0, right_wall_x=WIDTH)
 
-for i in range(20):
+for i in range(3):
     ball = RigidBody(position=Vector2(random.randint(25, WIDTH - 25), random.randint(100, 300)), mass=1.0, radius=25)
     balls.append(ball)
     world.add_body(ball)
@@ -27,8 +29,26 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.pos
+            click_position = Vector2(mouse_x, mouse_y)
+
+            for ball in balls:
+                direction = click_position - ball.position
+                distance_squared = direction.length_squared()
+
+                if distance_squared < ball.radius ** 2:
+                    dragged_ball = ball
+    
+        if event.type == pygame.MOUSEBUTTONUP:
+            dragged_ball = None
 
     world.step(dt)
+
+    if dragged_ball is not None:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        dragged_ball.position = Vector2(mouse_x, mouse_y)
 
     screen.fill((30, 30, 30))
 
@@ -44,5 +64,7 @@ while running:
     screen.blit(fps_text, (10, 10))
 
     pygame.display.flip()
+
+    
 
 pygame.quit()
